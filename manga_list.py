1127cabec82fe03bc43e72aad55697af0fe0f062
Jacob P Kaphingst <j8kap@hotmail.com>
2017-09-03 15:20:52 -0500

    Added manga_list.py which generates a file that seperates your manga list by type

--- manga_list.py
+++ manga_list.py
@@ -2,16 +2,14 @@ import spice_api as spice
 
 import re
 import os
-
-
+from collections import defaultdict
 import json
 import Agents
 
 import functools
 
 path=os.path.dirname(os.path.abspath(__file__))
-animeList=[]
-
+mangaList=defaultdict(list)
 with open(path+'/config.json') as data_file:
     config = json.load(data_file)
 
@@ -25,35 +23,38 @@ def catch_exception(f):
             try:
                 return f(*args, **kwargs)
             except Exception as e:
-                print(count)
+                pass
+
             count+=1
+
+        print(args[0])
     return func
 
 @catch_exception
 def scrapeInfo(showId,creds):
-    nameInfo = spice.search_id(showId,spice.get_medium('anime'),creds)
+    nameInfo = spice.search_id(showId,spice.get_medium('manga'),creds)
+    mangaList[nameInfo.manga_type].append(nameInfo.title)
 
-    if ('Currently Airing' != nameInfo.status):
-        name=re.sub(r'\([^)]*\)', '', nameInfo.title)
-        animeList.append(name)
 
 if __name__ == "__main__":
     agent = Agents.Agent(method=scrapeInfo,n_threads=20)
-    your_list = spice.get_list(spice.get_medium('anime'),creds[0] ,creds)
-    ids=your_list.get_status(1)
-    animeList.clear()
-    
+    your_list = spice.get_list(spice.get_medium('manga'),creds[0],creds)
+    ids=your_list.get_ids()
+    mangaList.clear()
+
     count = 1
     for id in ids:
         agent.execute_async(id,creds)
 
     agent.finalize()
-    animeList.sort()
+    final=""
+    for key in mangaList.keys():
+        mangaList[key].sort()
+        mangaList[key]=["\n"+key] + mangaList[key]
+        final+="\n".join(mangaList[key])
 
 
-    label =""
-    for show in animeList:
-        label+=show+'\n'
-    print(label)
-    thefile = open('animeList.txt', 'w')
-    thefile.write("\n".join(animeList))
+
+
+    thefile = open('mangaList.txt', 'w',encoding="utf8")
+    thefile.write(final[0:])
